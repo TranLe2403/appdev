35ee7e572c7b8786d47aab3c7c13317442b3da84
TranLe <trantran.0324@gmail.com>
2019-04-02 09:27:55 +0100

    modified main.c for recording

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include "screen.h" 
+#include "sound.h"
+#include <stdio.h>
 int main(){
-	int dec[COL];		//80.pieces of sound
-	srand(time(NULL));
-	for(int i=0; i<COL; i++) dec[i] = rand()%70+30;
-	clearscreen();
-	colors(RED, bg(YELLOW));
-	bar_chart(dec);
-//	printf("message from main\n");
+	FILE *f;
+	short sd[80000];
+	for(;;){
+		system(RCMD);
+		f = fopen("test.wav", "r");
+		if(f==NULL){
+			printf("cannot open the file\n");
+			return 1;
+		}
+		clearscreen();
+		colors(RED, bg(YELLOW));
+		struct WAVHDR hdr;
+		fread(&hdr, sizeof(hdr), 1, f);	// read WAV header
+		fread(&sd, sizeof(sd), 1, f);	//read wav data
+		fclose(f);
+		displayWAVHDR(hdr);
+		//displayWAVDATA();
+	}
 	reset_color();
 	getchar();	
 }
---
+++ sound.h
@@ -0,0 +1,27 @@
+//constants definitions
+#define RATE 16000
+#define DUR 5
+#define RCMD "arecord -r16000 -c1 -f S16_LE -d5 -q test.wav"
+// data structures
+struct WAVHDR{
+	char ChunkID[4];		//must be "RIFF"
+	int ChunkSize;
+	char Format[4];			//must be "WAVE"
+
+	char Subchunk1ID[4]; 	//must be "fmt "
+	int Subchunk1Size;		//should be 16
+	short AudioFormat;		//shld be 1
+	short NumChannels;		// should be 1
+	int SampleRate;			//we will use 16000
+	int ByteRate;			//calculate by sampleRate, NumChannels, B/s
+	short BlockAlign;		//Calculated
+	short BitsPerSample;	//should be 16
+
+	char Subchunk2ID[4];	//must be "data"
+	int Subchunk2Size;		//calculated
+};
+
+// function declarations
+void displayWAVHDR(struct WAVHDR);
+void showID(char *, char *);
+void displayWAVDATA(short []);
