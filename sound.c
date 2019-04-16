24b17b50cd506a4853c5e8b26b2f597418289d74
TranLe <trantran.0324@gmail.com>
2019-04-16 10:27:13 +0100

    testTone

--- sound.c
+++ sound.c
@@ -3,6 +3,63 @@
 #include "sound.h"
 #include <math.h>
 #include "screen.h"
+
+void fillID(char *d, const char *s){
+	for(int i=0; i<4; i++)	*d++ = *s++;
+}
+
+void testTone(int c, int fl, int fr, float d){
+	if(c<1 || c>2){
+		printf("Wrong num of channels.\n");
+		return;
+	}
+	if(fl<50 || fl>16000){
+		printf("Frequency is out of range\n");
+		return;
+	}
+	if(d<1.0 || d>10.0){
+		printf("Wrong duration\n");
+		return;
+	}
+
+	//All the argument are okay, the rest of code is to
+		//1) make a correct wave header
+		//2) generate correct samples
+		//3) write both header and samples to a file
+
+	struct WAVHDR h;
+	int samples = 44100*d, i;
+	fillID(h.ChunkID, "RIFF");
+	fillID(h.Format, "WAVE");
+	fillID(h.Subchunk1ID, "fmt");
+	fillID(h.Subchunk2ID, "data");
+	h.Subchunk1Size = 16; 	//constant value
+	h.AudioFormat = 1;
+	h.SampleRate = 44100;
+	h.BitsPerSample = 16;
+	h.BlockAlign = c*16/8;
+	h.NumChannels = c;
+	h.ByteRate = 44100*c*16/8;
+	h.Subchunk2Size = samples*c*16000/8;
+	h.ChunkSize = h.Subchunk2Size + 36;
+	FILE *fp = fopen ("testTone.wav", "w");
+	if(fp == NULL){
+		printf("Cannot open a file\n");
+		return;
+	}
+	fwrite(&h, sizeof(h), 1, fp);	// write rhe  header to file
+		// generate samples and write to file
+	for(i=0; i<samples; i++){
+		short sL = 32767.0*sin(2*PI*fl*i/44100);
+		fwrite(&sL, sizeof(short), 1, fp);
+		if(c==2){
+			short sR = 32767.0*sin(2*PI*fr*i/44100);
+			fwrite(&sR, sizeof(short), 1 ,fp);
+		}
+	}	//end of for
+	fclose(fp);	//close the file
+	printf("testTone is generated!\n");
+}
 void showID(char *idname,char *id){
 	int i;
 	printf("%s : ", idname);
